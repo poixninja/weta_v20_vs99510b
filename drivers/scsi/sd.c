5e4196cb80dda2f19f00632b12791f9d4f9df936
mentalmuso <mentalmuso@gmail.com>
2017-06-09 18:45:46 -0500

    Fix various issues preventing compile
    Change-Id: Iffafc0daddbd3a1eb35dd428a532177b5d0483d8
    
    (cherry picked from commit be9622b1de583b41e433c43cf125af5609ca8f5c)

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -1426,6 +1426,8 @@ static int media_not_present(struct scsi_disk *sdkp,
  *
  *	Note: this function is invoked from the block subsystem.
  **/
+
+/*
 static unsigned int sd_check_events(struct gendisk *disk, unsigned int clearing)
 {
 	struct scsi_disk *sdkp = scsi_disk(disk);
@@ -1436,18 +1438,20 @@ static unsigned int sd_check_events(struct gendisk *disk, unsigned int clearing)
 	printk("alex enter sd_check_events\n");
 
 	SCSI_LOG_HLQUEUE(3, sd_printk(KERN_INFO, sdkp, "sd_check_events\n"));
-
+*/
 	/*
 	 * If the device is offline, don't send any commands - just pretend as
 	 * if the command failed.  If the device ever comes back online, we
 	 * can deal with it then.  It is only because of unrecoverable errors
 	 * that we would ever take a device offline in the first place.
 	 */
+     
+/*
 	if (!scsi_device_online(sdp)) {
 		set_media_not_present(sdkp);
 		goto out;
 	}
-
+*/
 	/*
 	 * Using TEST_UNIT_READY enables differentiation between drive with
 	 * no cartridge loaded - NOT READY, drive with changed cartridge -
@@ -1457,42 +1461,43 @@ static unsigned int sd_check_events(struct gendisk *disk, unsigned int clearing)
 	 * by sd_spinup_disk() from sd_revalidate_disk(), which happens whenever
 	 * sd_revalidate() is called.
 	 */
-	retval = -ENODEV;
+/*
+retval = -ENODEV;
 
 	if (scsi_block_when_processing_errors(sdp)) {
 		sshdr  = kzalloc(sizeof(*sshdr), GFP_KERNEL);
 		retval = scsi_test_unit_ready(sdp, SD_TIMEOUT, SD_MAX_RETRIES,
 					      sshdr);
 	}
-
+*/
 	/* failed to execute TUR, assume media not present */
-	if (host_byte(retval)) {
+/*	if (host_byte(retval)) {
 		set_media_not_present(sdkp);
 		goto out;
 	}
 
 	if (media_not_present(sdkp, sshdr))
 		goto out;
-
+*/
 	/*
 	 * For removable scsi disk we have to recognise the presence
 	 * of a disk in the drive.
 	 */
-	if (!sdkp->media_present)
+/*	if (!sdkp->media_present)
 		sdp->changed = 1;
 	sdkp->media_present = 1;
-out:
+out: */
 	/*
 	 * sdp->changed is set under the following conditions:
 	 *
 	 *	Medium present state has changed in either direction.
 	 *	Device has indicated UNIT_ATTENTION.
 	 */
-	kfree(sshdr);
+/*	kfree(sshdr);
 	retval = sdp->changed ? DISK_EVENT_MEDIA_CHANGE : 0;
 	sdp->changed = 0;
 	return retval;
-}
+}*/
 #endif
 
 static int sd_sync_cache(struct scsi_disk *sdkp)
@@ -1607,9 +1612,6 @@ static const struct block_device_operations sd_fops = {
 	.getgeo			= sd_getgeo,
 #ifdef CONFIG_COMPAT
 	.compat_ioctl		= sd_compat_ioctl,
-#endif
-#ifdef CONFIG_MACH_LGE
-	.check_events           = sd_check_events,
 #endif
 	.revalidate_disk	= sd_revalidate_disk,
 	.unlock_native_capacity	= sd_unlock_native_capacity,
@@ -2982,11 +2984,7 @@ static int sd_media_scan_thread(void *__sdkp)
 			(sdkp->thread_remove && sdkp->async_end), 3*HZ);
 		if (sdkp->thread_remove && sdkp->async_end)
 			break;
-#ifdef CONFIG_MACH_LGE
-		ret = sd_check_events(sdkp->disk, 0);
-#else
 		ret = 0;
-#endif
 
 		if (sdkp->prv_media_present
 				!= sdkp->media_present) {
